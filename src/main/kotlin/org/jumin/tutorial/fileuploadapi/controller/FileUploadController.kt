e16898a52e9e2274d50da13c2f0b8eb6cd57eb58
JR <juminrubin@gmail.com>
2023-01-24 19:56:38 +0100

    add open-api to the controller

--- src/main/kotlin/org/jumin/tutorial/fileuploadapi/controller/FileUploadController.kt
+++ src/main/kotlin/org/jumin/tutorial/fileuploadapi/controller/FileUploadController.kt
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.util.stream.Collectors
 
-@Controller
+@RestController
 @CrossOrigin("http://localhost:8081")
+@RequestMapping("/api/files")
 class FileUploadController {
 
     @Autowired
@@ -33,7 +33,7 @@ class FileUploadController {
         }
     }
 
-    @GetMapping("/files")
+    @GetMapping("/")
     fun getListFiles(): ResponseEntity<List<ResponseFile>>? {
         val files: List<ResponseFile> = storageService?.getAllUploadedFiles()!!.map { uploadedFile ->
             val fileDownloadUri = ServletUriComponentsBuilder
@@ -51,7 +51,7 @@ class FileUploadController {
         return ResponseEntity.status(HttpStatus.OK).body<List<ResponseFile>>(files)
     }
 
-    @GetMapping("/files/{id}")
+    @GetMapping("/{id}")
     fun getFile(@PathVariable id: String): ResponseEntity<ByteArray?>? {
         val uploadedFile: UploadedFile? = storageService?.getUploadedFile(id)
         return ResponseEntity.ok()
